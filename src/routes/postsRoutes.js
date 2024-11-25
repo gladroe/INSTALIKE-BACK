55ecdc45929834c1634638de8a89ba6eed648119
Gladroe <herbertfags@gmail.com>
2024-11-25 00:32:45 -0300

    Adicionado o Multer para upload de arquivos.

--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,11 +1,31 @@
 import express from "express";
-import { litarPosts } from "../controllers/postsController.js";
+import multer from "multer";
+import {
+	listarPosts,
+	postarNovoPost,
+	uploadImagem,
+} from "../controllers/postsController.js";
+
+const storage = multer.diskStorage({
+	destination: function (req, file, cb) {
+		cb(null, "uploads/");
+	},
+	filename: function (req, file, cb) {
+		cb(null, file.originalname);
+	},
+});
+
+const upload = multer({ dest: "./uploads", storage });
 
 const routes = (app) => {
 	app.use(express.json());
 
 	// Definindo uma rota GET para buscar todos os posts
-	app.get("/posts", litarPosts);
+	app.get("/posts", listarPosts);
+	// Rota para criar um novo post
+	app.post("/posts", postarNovoPost);
+
+	app.post("/upload", upload.single("imagem"), uploadImagem);
 };
 
 export default routes;
